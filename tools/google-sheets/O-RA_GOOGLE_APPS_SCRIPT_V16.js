3b9184a57e0c14baf92a1ba5d27a87e909915b04
orastorelk-cell <orastore.lk@gmail.com>
2026-08-20 03:00:20 +0530

    Remove incomplete Google Sheets V16 placeholder

--- tools/google-sheets/O-RA_GOOGLE_APPS_SCRIPT_V16.js
+++
@@ -1,5 +0,0 @@
-// O-RA Store Google Sheet Sync V16.0
-// Full production Apps Script is maintained here as the single source of truth.
-// Paste the full contents of this file into the Google Sheet Apps Script editor.
-
-"+"
