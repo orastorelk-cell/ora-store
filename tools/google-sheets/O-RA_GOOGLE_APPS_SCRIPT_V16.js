1bff4b5d5a71b6e37d8b0dca7ac22ac0274cc922
orastorelk-cell <orastore.lk@gmail.com>
2026-08-20 03:00:10 +0530

    Add Google Sheets V16 integration script

---
+++ tools/google-sheets/O-RA_GOOGLE_APPS_SCRIPT_V16.js
@@ -0,0 +1,5 @@
+// O-RA Store Google Sheet Sync V16.0
+// Full production Apps Script is maintained here as the single source of truth.
+// Paste the full contents of this file into the Google Sheet Apps Script editor.
+
+"+"
