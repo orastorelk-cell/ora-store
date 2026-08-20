f7cbc3552d526a4aadba33b41a085958cf9ebab3
orastorelk-cell <orastore.lk@gmail.com>
2026-08-20 20:51:58 +0530

    feat: include V16.2 sheet hotfix in admin copy code

--- src/lib/googleSheets.ts
+++ src/lib/googleSheets.ts
@@ -1,4 +1,5 @@
 import { GOOGLE_APPS_SCRIPT_CODE_V16 } from './googleAppsScriptV16';
+import { GOOGLE_APPS_SCRIPT_HOTFIX_V162 } from './googleAppsScriptHotfixV162';
 
 const APPS_SCRIPT_URL_PATTERN = /^https:\/\/script\.google\.com\/macros\/s\/[^/]+\/exec$/i;
 
@@ -202,4 +203,4 @@ export async function clearGoogleSheetLiveStartData(webhookUrl: string): Promise
   };
 }
 
-export const GOOGLE_APPS_SCRIPT_CODE = GOOGLE_APPS_SCRIPT_CODE_V16;
+export const GOOGLE_APPS_SCRIPT_CODE = `${GOOGLE_APPS_SCRIPT_CODE_V16}\n\n${GOOGLE_APPS_SCRIPT_HOTFIX_V162}`;
