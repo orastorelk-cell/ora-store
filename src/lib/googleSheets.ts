b6a8ae6738946f924ccfce4beb84fda839740cec
orastorelk-cell <orastore.lk@gmail.com>
2026-08-22 01:42:36 +0530

    feat: wire action chip visuals into Apps Script bundle

--- src/lib/googleSheets.ts
+++ src/lib/googleSheets.ts
@@ -4,6 +4,7 @@ import { GOOGLE_APPS_SCRIPT_CALL_CENTER_UX } from './googleAppsScriptCallCenterU
 import { GOOGLE_APPS_SCRIPT_STABLE_ROWS } from './googleAppsScriptStableRows';
 import { GOOGLE_APPS_SCRIPT_FULL_RESET } from './googleAppsScriptFullReset';
 import { GOOGLE_APPS_SCRIPT_VARIANT_PRICE } from './googleAppsScriptVariantPrice';
+import { GOOGLE_APPS_SCRIPT_ACTION_CHIPS } from './googleAppsScriptActionChips';
 
 const APPS_SCRIPT_URL_PATTERN = /^https:\/\/script\.google\.com\/macros\/s\/[^/]+\/exec$/i;
 
@@ -202,4 +203,4 @@ export async function clearGoogleSheetLiveStartData(webhookUrl: string): Promise
   return { success: true, message: 'Google Sheet order data cleared.', removed: Number(posted.result?.removed || 0), status: posted.result?.status, version: posted.result?.version };
 }
 
-export const GOOGLE_APPS_SCRIPT_CODE = `${GOOGLE_APPS_SCRIPT_CODE_CLEAN_V1}\n\n${GOOGLE_APPS_SCRIPT_CITY_EXACT_8549}\n\n${GOOGLE_APPS_SCRIPT_CALL_CENTER_UX}\n\n${GOOGLE_APPS_SCRIPT_STABLE_ROWS}\n\n${GOOGLE_APPS_SCRIPT_FULL_RESET}\n\n${GOOGLE_APPS_SCRIPT_VARIANT_PRICE}`;
+export const GOOGLE_APPS_SCRIPT_CODE = `${GOOGLE_APPS_SCRIPT_CODE_CLEAN_V1}\n\n${GOOGLE_APPS_SCRIPT_CITY_EXACT_8549}\n\n${GOOGLE_APPS_SCRIPT_CALL_CENTER_UX}\n\n${GOOGLE_APPS_SCRIPT_STABLE_ROWS}\n\n${GOOGLE_APPS_SCRIPT_FULL_RESET}\n\n${GOOGLE_APPS_SCRIPT_VARIANT_PRICE}\n\n${GOOGLE_APPS_SCRIPT_ACTION_CHIPS}`;
