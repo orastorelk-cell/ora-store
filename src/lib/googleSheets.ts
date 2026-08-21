8e2f25aa1f56e4c7ccf30d72918c8e29644ba455
orastorelk-cell <orastore.lk@gmail.com>
2026-08-22 04:15:24 +0530

    perf: include bulk lead speed v2 in Apps Script bundle

--- src/lib/googleSheets.ts
+++ src/lib/googleSheets.ts
@@ -8,6 +8,7 @@ import { GOOGLE_APPS_SCRIPT_ACTION_CHIPS } from './googleAppsScriptActionChips';
 import { GOOGLE_APPS_SCRIPT_PRICE_FORMAT } from './googleAppsScriptPriceFormat';
 import { GOOGLE_APPS_SCRIPT_ORDER_DETAILS } from './googleAppsScriptOrderDetails';
 import { GOOGLE_APPS_SCRIPT_BULK_FAST } from './googleAppsScriptBulkFast';
+import { GOOGLE_APPS_SCRIPT_BULK_SPEED_V2 } from './googleAppsScriptBulkSpeedV2';
 
 const APPS_SCRIPT_URL_PATTERN = /^https:\/\/script\.google\.com\/macros\/s\/[^/]+\/exec$/i;
 
@@ -206,4 +207,4 @@ export async function clearGoogleSheetLiveStartData(webhookUrl: string): Promise
   return { success: true, message: 'Google Sheet order data cleared.', removed: Number(posted.result?.removed || 0), status: posted.result?.status, version: posted.result?.version };
 }
 
-export const GOOGLE_APPS_SCRIPT_CODE = `${GOOGLE_APPS_SCRIPT_CODE_CLEAN_V1}\n\n${GOOGLE_APPS_SCRIPT_CITY_EXACT_8549}\n\n${GOOGLE_APPS_SCRIPT_CALL_CENTER_UX}\n\n${GOOGLE_APPS_SCRIPT_STABLE_ROWS}\n\n${GOOGLE_APPS_SCRIPT_FULL_RESET}\n\n${GOOGLE_APPS_SCRIPT_VARIANT_PRICE}\n\n${GOOGLE_APPS_SCRIPT_ACTION_CHIPS}\n\n${GOOGLE_APPS_SCRIPT_PRICE_FORMAT}\n\n${GOOGLE_APPS_SCRIPT_ORDER_DETAILS}\n\n${GOOGLE_APPS_SCRIPT_BULK_FAST}`;
+export const GOOGLE_APPS_SCRIPT_CODE = `${GOOGLE_APPS_SCRIPT_CODE_CLEAN_V1}\n\n${GOOGLE_APPS_SCRIPT_CITY_EXACT_8549}\n\n${GOOGLE_APPS_SCRIPT_CALL_CENTER_UX}\n\n${GOOGLE_APPS_SCRIPT_STABLE_ROWS}\n\n${GOOGLE_APPS_SCRIPT_FULL_RESET}\n\n${GOOGLE_APPS_SCRIPT_VARIANT_PRICE}\n\n${GOOGLE_APPS_SCRIPT_ACTION_CHIPS}\n\n${GOOGLE_APPS_SCRIPT_PRICE_FORMAT}\n\n${GOOGLE_APPS_SCRIPT_ORDER_DETAILS}\n\n${GOOGLE_APPS_SCRIPT_BULK_FAST}\n\n${GOOGLE_APPS_SCRIPT_BULK_SPEED_V2}`;
