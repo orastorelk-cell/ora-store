d5995980b4875f5b9d522fb688a32363db73adf8
orastorelk-cell <orastore.lk@gmail.com>
2026-09-04 12:12:30 +0530

    Use reliable awaited Facebook lead recovery

--- worker/indexV3LeadRepair.ts
+++ worker/indexV3LeadRepair.ts
@@ -1,12 +1,12 @@
 import baseWorker from './indexV3';
-import { scheduleFacebookLeadRecoveryOnce } from './facebookLeadRecoveryOnce';
+import { scheduleFacebookLeadRecoveryLive } from './facebookLeadRecoveryLive';
 
 export default {
   async fetch(request: Request, env: unknown, ctx: any) {
-    scheduleFacebookLeadRecoveryOnce(baseWorker, env, ctx);
+    scheduleFacebookLeadRecoveryLive(baseWorker, env, ctx);
     return baseWorker.fetch(request, env, ctx);
   },
   async scheduled(_controller: unknown, env: unknown, ctx: any) {
-    scheduleFacebookLeadRecoveryOnce(baseWorker, env, ctx);
+    scheduleFacebookLeadRecoveryLive(baseWorker, env, ctx);
   },
 };
