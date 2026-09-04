757505584f6d9a2c50752e6f536466a44254c15a
orastorelk-cell <orastore.lk@gmail.com>
2026-09-04 11:36:29 +0530

    Add isolated Facebook lead repair wrapper

---
+++ worker/indexV3LeadRepair.ts
@@ -0,0 +1,9 @@
+import baseWorker from './indexV3';
+import { scheduleFacebookLeadRecoveryOnce } from './facebookLeadRecoveryOnce';
+
+export default {
+  async fetch(request: Request, env: unknown, ctx: any) {
+    scheduleFacebookLeadRecoveryOnce(baseWorker, env, ctx);
+    return baseWorker.fetch(request, env, ctx);
+  },
+};
