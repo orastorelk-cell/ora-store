3615357f0ef3631927ca59499f4175398471329c
orastorelk-cell <orastore.lk@gmail.com>
2026-09-04 12:23:07 +0530

    Retry missing FB-000067 Google Sheet row

--- worker/indexV3LeadRepair.ts
+++ worker/indexV3LeadRepair.ts
@@ -1,12 +1,15 @@
 import baseWorker from './indexV3';
 import { scheduleFacebookLeadRecoveryLive } from './facebookLeadRecoveryLive';
+import { scheduleFacebookLeadSheetRepairOnce } from './facebookLeadSheetRepairOnce';
 
 export default {
   async fetch(request: Request, env: unknown, ctx: any) {
     scheduleFacebookLeadRecoveryLive(baseWorker, env, ctx);
+    scheduleFacebookLeadSheetRepairOnce(baseWorker, env, ctx);
     return baseWorker.fetch(request, env, ctx);
   },
   async scheduled(_controller: unknown, env: unknown, ctx: any) {
     scheduleFacebookLeadRecoveryLive(baseWorker, env, ctx);
+    scheduleFacebookLeadSheetRepairOnce(baseWorker, env, ctx);
   },
 };
